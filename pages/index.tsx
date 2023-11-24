76dae2e5117017dca7876f992e65a7dcc9a1605f
Elar Saks <elarsaks@gmail.com>
2023-11-24 23:03:37 +0200

    Create circular menu.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
+import CircularMenu from "@/components/CircularMenu";
 import ParticlesCanvas from "@/components/ParticlesCanvas";
 
 export default function () {
   return (
     <div>
       <ParticlesCanvas />
+      <CircularMenu />
 
       {/* Other components */}
     </div>
