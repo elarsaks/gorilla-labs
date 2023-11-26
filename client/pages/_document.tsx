5e1db25b0cf3de18334c104023a7eeb4931ff2b0
Elar Saks <elarsaks@gmail.com>
2023-11-26 09:02:58 +0200

    Add favicon.

--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -1,24 +1,32 @@
-import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
 
 class MyDocument extends Document {
-    static async getInitialProps(ctx: DocumentContext) {
-        const initialProps = await Document.getInitialProps(ctx);
-        return { ...initialProps };
-    }
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
 
-    render() {
-        return (
-            <Html lang="en">
-                <Head>
-                    {/* Here you can add global scripts, stylesheets, meta tags, etc. */}
-                </Head>
-                <body>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        );
-    }
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <title>Your Site Title</title>
+          <link rel="icon" href="/favicon/favicon.ico" />
+          {/* Here you can add global scripts, stylesheets, meta tags, etc. */}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
 
 export default MyDocument;
