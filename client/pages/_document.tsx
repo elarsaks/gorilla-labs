819f99e3ab1f48472786acf1bcd5d8c92e937790
Elar Saks <elarsaks@gmail.com>
2023-11-27 11:18:00 +0200

    Create image for some.

--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -17,6 +17,21 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="icon" href="/favicon/favicon.ico" />
+
+          <meta name="title" content="Your Website Title" />
+          <meta name="description" content="A brief description of your page" />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://gorilla-labs-sandbox.org" />
+          <meta property="og:title" content="Your Website Title" />
+          <meta
+            property="og:description"
+            content="A brief description of your page"
+          />
+          <meta
+            property="og:image"
+            content="https://gorilla-labs-sandbox.org/some.png"
+          />
+
           {/* Here you can add global scripts, stylesheets, meta tags, etc. */}
         </Head>
         <body>
