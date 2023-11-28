c9141e58e901c8846bac2932ba2953fae594b125
Elar Saks <elarsaks@gmail.com>
2023-11-28 12:00:44 +0200

    Add sign in directly via Facebook.

--- client/pages/api/auth/[...nextauth].js
+++ client/pages/api/auth/[...nextauth].js
@@ -1,13 +1,14 @@
 import NextAuth from 'next-auth'
-import Providers from 'next-auth/providers'
+import FacebookProvider from "next-auth/providers/facebook";
 
 export default NextAuth({
     providers: [
-        Providers.Cognito({
-            clientId: process.env.COGNITO_CLIENT_ID,
-            clientSecret: process.env.COGNITO_CLIENT_SECRET,
-            domain: process.env.COGNITO_DOMAIN
-        })
-    ]
+        FacebookProvider({
+            clientId: process.env.FACEBOOK_CLIENT_ID,
+            clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+        }),
+    ],
+
+
 })
 
