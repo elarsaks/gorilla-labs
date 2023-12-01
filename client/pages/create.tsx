2575ded2c94f76b9936f89cba63bb36a602c3b17
Elar Saks <elarsaks@gmail.com>
2023-12-01 14:05:45 +0200

    Start styling create page.

--- client/pages/create.tsx
+++ client/pages/create.tsx
@@ -5,32 +5,60 @@ export const Form = styled.form`
   display: flex;
   flex-direction: column;
   gap: 20px;
-  max-width: 500px;
-  margin: auto;
+  max-width: 800px;
+  min-width: 40vw;
+  margin: 10vh auto;
+  padding: 20px;
+  padding-top: 0px;
+  border-radius: 8px;
+  border: 1px solid aqua;
+  background-color: rgba(0, 0, 0, 0.7);
 `;
 
 export const Input = styled.input`
-  padding: 10px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
+  padding: 12px;
+  border: 2px solid #ccc;
+  border-radius: 8px;
+  border: 1px solid aqua;
+  font-size: 16px;
+
+  &:focus {
+    border-color: #a4c2f4;
+    outline: none;
+  }
+
+  &[type="file"] {
+    border: none;
+  }
 `;
 
 export const TextArea = styled.textarea`
-  padding: 10px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
+  padding: 12px;
+  border: 2px solid #ccc;
+  border-radius: 8px;
+  border: 1px solid aqua;
+  font-size: 16px;
+  min-height: 100px;
+  resize: vertical;
+
+  &:focus {
+    border-color: #a4c2f4;
+    outline: none;
+  }
 `;
 
 export const Button = styled.button`
   padding: 10px 20px;
   border: none;
-  border-radius: 5px;
-  background-color: blue;
+  border-radius: 8px;
+  background-color: #007bff;
   color: white;
+  font-size: 16px;
   cursor: pointer;
+  transition: background-color 0.2s;
 
   &:hover {
-    background-color: darkblue;
+    background-color: #0056b3;
   }
 `;
 
@@ -47,12 +75,15 @@ const CreateNFT: React.FC = () => {
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    // Handle the form submission, e.g., upload to IPFS and mint NFT
     console.log({ title, description, image });
   };
 
   return (
     <Form onSubmit={handleSubmit}>
+      <h1 style={{ color: "white", backgroundColor: "rgba(0, 0, 0, 0.2)" }}>
+        Create NFT
+      </h1>{" "}
+      {/* Header styled white */}
       <Input
         type="text"
         placeholder="NFT Title"
