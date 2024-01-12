9660a5f8cc0ec5a7d8a7e41bc140e99709c976e0
Elar Saks <elarsaks@gmail.com>
2024-01-12 12:08:39 +0200

    Add more settings into linkedin login.

--- client/pages/api/auth/[...nextauth].ts
+++ client/pages/api/auth/[...nextauth].ts
@@ -7,7 +7,21 @@ export default NextAuth({
         LinkedInProvider({
             clientId: process.env.NEXT_LINKEDIN_CLIENT_ID || '',
             clientSecret: process.env.NEXT_LINKEDIN_CLIENT_SECRET || '',
-
+            authorization: {
+                params: { scope: 'openid profile email' },
+            },
+            issuer: 'https://www.linkedin.com',
+            jwks_endpoint: 'https://www.linkedin.com/oauth/openid/jwks',
+            profile(profile, tokens) {
+                const defaultImage =
+                    'https://cdn-icons-png.flaticon.com/512/174/174857.png';
+                return {
+                    id: profile.sub,
+                    name: profile.name,
+                    email: profile.email,
+                    image: profile.picture ?? defaultImage,
+                };
+            },
 
         }),
         // ...add other providers if needed
