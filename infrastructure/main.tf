93a56f478d86188f552288710d9e92f926a8344e
Elar Saks <elarsaks@gmail.com>
2024-01-18 08:44:45 +0200

    Start creating Nginx CICD.

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -81,14 +81,6 @@ resource "aws_instance" "gorilla_labs" {
             proxy_set_header X-Real-IP $remote_addr;
         }
 
-        location /socket.io/ {
-            proxy_pass http://localhost:3000;
-            proxy_http_version 1.1;
-            proxy_set_header Upgrade $http_upgrade;
-            proxy_set_header Connection 'upgrade';
-            proxy_set_header Host $host;
-            proxy_cache_bypass $http_upgrade;
-        }
     }
     EOL
 
