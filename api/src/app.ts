45de40b5b8b72a8f8079a4e4af88a89bc5be8c30
Elar Saks <elarsaks@gmail.com>
2024-01-26 22:33:20 +0200

    Add router.

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,11 +1,11 @@
 import express from 'express';
+import router from './router';
 
 const app = express();
-const port = 4000; // You can choose the port you want
+const port = 4000;
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-});
+app.use(express.json());
+app.use('/', router);
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
