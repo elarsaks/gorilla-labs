717de7ecfd46495e158ea485bfcf2427b846d832
Elar Saks <elarsaks@gmail.com>
2024-02-04 13:57:35 +0200

    Open port 4000 in terraform.

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -38,6 +38,14 @@ resource "aws_security_group" "gorilla_labs_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "Custom HTTP on port 4000"
+    from_port   = 4000
+    to_port     = 4000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
